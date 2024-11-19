edf86e7862270b6b65a395b7c82cf60413ca61fe
Godwin Nwalozie <godwin.nwalozie@gmail.com>
2024-11-19 21:45:10 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -104,9 +104,9 @@ with st.container():
         with st.container():
             
             
-            st.write(" Enter a sample tweet or product review to test")
+            st.write("###### Enter a sample tweet or product review to test")
             
-            tweet = st.text_input(' delete review to input yours', 'sample: the customer service is very poor and they delayed in fixing my issues ') 
+            tweet = st.text_input('delete review to input yours', 'sample: the customer service is very poor and they delayed in fixing my issues ') 
             
 
             if st.button('click to make a prediction 👈'):
