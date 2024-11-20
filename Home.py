996f538b361e1378a80ac1852aec6b8c2e95f59b
Godwin Nwalozie <godwin.nwalozie@gmail.com>
2024-11-20 15:39:50 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -178,7 +178,7 @@ with col2:
         fig, ax = plt.subplots() 
         super = dataset.loc[:,["tweets","airline_sentiment"]]
         text = "".join(super[super.airline_sentiment == "negative"].tweets.astype('str'))
-        wc= WordCloud(max_words = 2000,background_color="orange", max_font_size=100, scale=10,\
+        wc= WordCloud(max_words = 2000,background_color="gold", max_font_size=100, scale=10,\
     relative_scaling=.6,random_state=42,normalize_plurals=True).generate(text)
         plt.title("Wordcloud | Most recurring negative words", fontsize = 17)
         plt.axis("off")
