1749d4fe4c0941b97b9ace23075b14ed9479744d
Godwin Nwalozie <godwin.nwalozie@gmail.com>
2024-11-25 16:34:13 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -173,7 +173,7 @@ with st.container():
     def show_dataset ():
         if st.button("randomize dataset"):
             random.random()
-        return st.write(dataset.sample(4))  
+        #return st.write(dataset.sample(4))  
     plot7 = show_dataset()
     
 
